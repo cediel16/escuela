d63fd7842c6659c4150c907cb490edeadef6c944
Johel Cediel <cediel16@gmail.com>
2013-09-24 21:18:35 -0430

    Creado formulario para agregar plantel

--- application/views/planteles/add.php
+++ application/views/planteles/add.php
@@ -1,51 +1,94 @@
 <?php
-$usuario = array(
+$rif = array(
     'type' => 'input',
-    'id' => 'usuario',
-    'name' => 'usuario',
+    'id' => 'rif',
+    'name' => 'rif',
     'class' => 'form-control'
 );
 
-$clave = array(
-    'type' => 'password',
-    'id' => 'clave',
-    'name' => 'clave',
-    'class' => 'form-control'
-);
-
-$conf_clave = array(
-    'type' => 'password',
-    'id' => 'conf_clave',
-    'name' => 'conf_clave',
-    'class' => 'form-control'
-);
-
-$email = array(
+$nombre_plantel = array(
     'type' => 'input',
-    'id' => 'email',
-    'name' => 'email',
+    'id' => 'nombre_plantel',
+    'name' => 'nombre_plantel',
+    'class' => 'form-control'
+);
+
+$direccion = array(
+    'id' => 'direccion',
+    'name' => 'direccion',
     'class' => 'form-control',
+    'rows' => 2
 );
 
-$grupo = array(
-    'id' => 'grupo',
-    'name' => 'grupo',
-    'options' => $opt_grupos,
+$estado = array(
+    'id' => 'estado',
+    'name' => 'estado',
+    'options' => array(),
     'selected' => '',
     'class' => "form-control"
 );
 
-$rol = array(
-    'id' => 'rol',
-    'name' => 'rol',
-    'options' => $opt_roles,
+$municipio = array(
+    'id' => 'municipio',
+    'name' => 'municipio',
+    'options' => array(),
     'selected' => '',
     'class' => "form-control"
 );
 
-$btnGuardarUsuario = array(
-    'name' => 'btnGuardarUsuario',
-    'id' => 'btnGuardarUsuario',
+$parroquia = array(
+    'id' => 'parroquia',
+    'name' => 'parroquia',
+    'options' => array(),
+    'selected' => '',
+    'class' => "form-control"
+);
+
+$telefono_plantel = array(
+    'type' => 'input',
+    'id' => 'telefono_plantel',
+    'name' => 'telefono_plantel',
+    'class' => 'form-control'
+);
+
+$email_plantel = array(
+    'type' => 'input',
+    'id' => 'email_plantel',
+    'name' => 'email_plantel',
+    'class' => 'form-control'
+);
+
+$cedula_director = array(
+    'type' => 'input',
+    'id' => 'cedula_director',
+    'name' => 'cedula_director',
+    'class' => 'form-control'
+);
+
+$nombre_director = array(
+    'type' => 'input',
+    'id' => 'nombre_director',
+    'name' => 'nombre_director',
+    'class' => 'form-control'
+);
+
+$telefono_director = array(
+    'type' => 'input',
+    'id' => 'telefono_director',
+    'name' => 'telefono_director',
+    'class' => 'form-control'
+);
+
+$email_director = array(
+    'type' => 'input',
+    'id' => 'email_director',
+    'name' => 'email_director',
+    'class' => 'form-control'
+);
+
+$btnGuardarPlantel = array(
+    'name' => 'btnGuardarPlantel',
+    'id' => 'btnGuardarPlantel',
     'content' => '<span class="icon icon-save"></span> Guardar',
     'class' => 'btn btn-success btn-sm'
 );
@@ -55,7 +98,7 @@ $btnGuardarUsuario = array(
         <div class="row">
             <div class="col-md-12">
                 <div class="btn-group">
-                    <?php echo form_button($btnGuardarUsuario) ?>
+                    <?php echo form_button($btnGuardarPlantel) ?>
                 </div>
                 <div class="btn-group">
                     <?php echo anchor('planteles', '<span class="text-danger"><span class="icon icon-remove-sign"></span></span> Cancelar', 'class="btn btn-default btn-sm"') ?>
@@ -78,57 +121,65 @@ $btnGuardarUsuario = array(
                 <?php echo form_open('', array('id' => 'frmUsuario', 'role' => 'form')) ?>
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="form-group">
-                            <label>RIF</label>
-                            <input type="text" class="form-control">
+                        <div class="form-group" id="form-group-rif">
+                            <label for="<?php echo $rif['id'] ?>">RIF</label>
+                            <?php echo form_input($rif) ?>
+                            <div id="form-msj-rif"></div>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Nombre</label>
-                            <input type="text" class="form-control">
+                        <div class="form-group" id="form-group-nombre-plantel">
+                            <label for="<?php echo $nombre_plantel['id'] ?>">Nombre</label>
+                            <?php echo form_input($nombre_plantel) ?>
+                            <div id="form-msj-nombre-plantel"></div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="form-group">
-                            <label>Dirección</label>
-                            <textarea class="form-control"></textarea>
+                        <div class="form-group" id="form-group-direccion">
+                            <label for="<?php echo $direccion['id'] ?>">Dirección</label>
+                            <?php echo form_textarea($direccion) ?>
+                            <div id="form-msj-direccion"></div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Estado</label>
-                            <select class="form-control"></select>
+                        <div class="form-group" id="form-group-estado">
+                            <label for="<?php echo $estado['id'] ?>">Estado</label>
+                            <?php echo form_dropdown($estado['name'], $estado['options'], $estado['selected'], 'id="' . $estado['id'] . '" class="' . $estado['class'] . '"') ?>
+                            <div id="form-msj-estado"></div>
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Municipio</label>
-                            <select class="form-control"></select>
+                        <div class="form-group" id="form-group-municipio">
+                            <label for="<?php echo $municipio['id'] ?>">Municipio</label>
+                            <?php echo form_dropdown($municipio['name'], $municipio['options'], $municipio['selected'], 'id="' . $municipio['id'] . '" class="' . $municipio['class'] . '"') ?>
+                            <div id="form-msj-municipio"></div>
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Parroquia</label>
-                            <select class="form-control"></select>
+                        <div class="form-group" id="form-group-parroquia">
+                            <label for="<?php echo $parroquia['id'] ?>">Parroquia</label>
+                            <?php echo form_dropdown($parroquia['name'], $parroquia['options'], $parroquia['selected'], 'id="' . $municipio['id'] . '" class="' . $parroquia['class'] . '"') ?>
+                            <div id="form-msj-parroquia"></div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Teléfono</label>
-                            <input type="text" class="form-control" />
+                        <div class="form-group" id="form-group-telefono-plantel">
+                            <label for="<?php echo $telefono_plantel['id'] ?>">Teléfono</label>
+                            <?php echo form_input($telefono_plantel) ?>
+                            <div id="form-msj-telefono-plantel"></div>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Correo electrónico</label>
-                            <input type="text" class="form-control" />
+                        <div class="form-group" id="form-group-email-plantel">
+                            <label for="<?php echo $email_plantel['id'] ?>">Correo electrónico</label>
+                            <?php echo form_input($email_plantel) ?>
+                            <div id="form-msj-email-plantel"></div>
                         </div>
                     </div>
                 </div>
@@ -137,23 +188,26 @@ $btnGuardarUsuario = array(
             <div class="col-md-3">
                 <legend>Datos del Director</legend>
                 <div class="row">
-
                     <div class="col-md-12">
-                        <div class="form-group">
-                            <label>Cédula</label>
-                            <input type="text" class="form-control">
+                        <div class="form-group" id="form-group-cedula-director">
+                            <label for="<?php echo $cedula_director['id'] ?>">Cédula</label>
+                            <?php echo form_input($cedula_director) ?>
+                            <div id="form-msj-cedula-director"></div>
                         </div>
-                        <div class="form-group">
-                            <label>Nombre</label>
-                            <input type="text" class="form-control">
+                        <div class="form-group" id="form-group-nombre-director">
+                            <label for="<?php echo $nombre_director['id'] ?>">Teléfono</label>
+                            <?php echo form_input($nombre_director) ?>
+                            <div id="form-msj-nombre-director"></div>
                         </div>
-                        <div class="form-group">
-                            <label>Teléfono</label>
-                            <input type="text" class="form-control">
+                        <div class="form-group" id="form-group-telefono-director">
+                            <label for="<?php echo $telefono_director['id'] ?>">Teléfono</label>
+                            <?php echo form_input($telefono_director) ?>
+                            <div id="form-msj-telefono-director"></div>
                         </div>
-                        <div class="form-group">
-                            <label>Correo electrónico</label>
-                            <input type="text" class="form-control">
+                        <div class="form-group" id="form-group-email-director">
+                            <label for="<?php echo $email_director['id'] ?>">Correo electrónico</label>
+                            <?php echo form_input($email_director) ?>
+                            <div id="form-msj-email-director"></div>
                         </div>
                     </div>
                 </div>
